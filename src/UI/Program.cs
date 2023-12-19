bf431de4399f80897a4179c6b1a8f66f62c27ad2
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-12-19 19:28:55 +0000

    update ui

--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -6,4 +6,12 @@ const bool isNailed = true;
 
 var africanParrot = new Parrot(ParrotTypes.African, numberOfCoconuts, voltage, isNailed);
 var africanSpeed = africanParrot.GetSpeed();
-Console.WriteLine(africanSpeed);
+Console.WriteLine($"African speed: {africanSpeed}");
+
+var norwegianBlue = new Parrot(ParrotTypes.NorwegianBlue, numberOfCoconuts, voltage, isNailed);
+var norwegianBlueSpeed = norwegianBlue.GetSpeed();
+Console.WriteLine($"NorwegianBlue speed: {norwegianBlueSpeed}");
+
+var europeanParrot = new Parrot(ParrotTypes.European, numberOfCoconuts, voltage, isNailed);
+var europeanSpeed = europeanParrot.GetSpeed();
+Console.WriteLine($"European speed: {europeanSpeed}");
