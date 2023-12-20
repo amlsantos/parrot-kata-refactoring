1b71e49876dcd159376f7dd1a57e11fa9e7ae85d
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-12-20 15:04:40 +0000

    add african parrot

---
+++ src/Domain/AfricanParrot.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain;
+
+public class AfricanParrot : Parrot
+{
+    private readonly int _numberOfCoconuts;
+
+    public AfricanParrot(ParrotTypes type, int numberOfCoconuts, double voltage, bool isNailed) 
+        : base(type, numberOfCoconuts, voltage, isNailed)
+    {
+        _numberOfCoconuts = numberOfCoconuts;
+    }
+
+    protected override ParrotTypes GetType() => ParrotTypes.African;
+
+    public override double GetSpeed() => Math.Max(0, GetBaseSpeed() - GetLoadFactor() * _numberOfCoconuts);
+
+    private double GetLoadFactor() => 9.0;
+}
